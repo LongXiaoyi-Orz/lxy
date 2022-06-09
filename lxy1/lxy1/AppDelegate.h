eedc8d80924a51dd829068c6da2162dd1de79f2f
我才是龙小易 <354943749@qq.com>
2022-06-09 10:48:12 +0800

    第一次提交

---
+++ lxy1/lxy1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lxy1
+//
+//  Created by mac on 2022/6/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ lxy1/lxy1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  lxy1
+//
+//  Created by mac on 2022/6/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
